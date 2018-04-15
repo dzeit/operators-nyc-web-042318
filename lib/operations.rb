c73a89b2b7993290647c54fea8d6af9164a23cca
David Zeit <dzeit216@gmail.com>
2018-04-15 16:18:26 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,7 +9,7 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-  if speed > 60 ? true : false
-  if speed < 40 ? true : false
+  if speed > 60 ? unsafe : safe
+  if speed < 40 ? unsafe : safe
   end
 end
